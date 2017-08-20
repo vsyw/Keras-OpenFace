37e3c5a7ee4264021831ec7767386f3b51ce334a
victor wang <iwantooxxoox@gmail.com>
2017-08-20 16:14:09 +0800

    Generate weight automatically

---
+++ tmp2.py
@@ -0,0 +1,13 @@
+Weights = [
+  'conv1', 'bn1', 'conv2', 'bn2', 'conv3', 'bn3',
+  'inception_3a_1x1_conv', 'inception_3a_1x1_bn',
+  'inception_3a_pool_conv', 'inception_3a_pool_bn',
+  'inception_3a_5x5_conv1', 'inception_3a_5x5_conv2', 'inception_3a_5x5_bn1', 'inception_3a_5x5_bn2',
+  'inception_3a_3x3_conv1', 'inception_3a_3x3_conv2', 'inception_3a_3x3_bn1', 'inception_3a_3x3_bn2',
+]
+
+for w in Weights:
+  if 'conv' in w:
+    print w
+  else:
+    print 'fuck'
