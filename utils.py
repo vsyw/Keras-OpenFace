35963200795d6c06e0db85ea95a04ac74ba6e2ca
victor wang <iwantooxxoox@gmail.com>
2017-08-21 21:39:38 +0800

    Complete the convertion

--- utils.py
+++ utils.py
@@ -29,25 +29,25 @@ def concatenate(tensors, axis=-1):
 
 def conv2d_bn(
   x,
-  cv1_name=None,
+  layer=None,
   cv1_out=None,
   cv1_filter=(1, 1),
   cv1_strides=(1, 1),
-  cv2_name=None,
   cv2_out=None,
   cv2_filter=(3, 3),
   cv2_strides=(1, 1),
-  padding=(1, 1),
-  bn1_name=None,
-  bn2_name=None
+  padding=None,
 ):
-  tensor = Conv2D(cv1_out, cv1_filter, strides=cv1_strides, data_format='channels_first', name=cv1_name)(x)
-  tensor = BatchNormalization(axis=1, epsilon=0.00001, name=bn1_name)(tensor)
+  num = '' if cv2_out == None else '1'
+  tensor = Conv2D(cv1_out, cv1_filter, strides=cv1_strides, data_format='channels_first', name=layer+'_conv'+num)(x)
+  tensor = BatchNormalization(axis=1, epsilon=0.00001, name=layer+'_bn'+num)(tensor)
   tensor = Activation('relu')(tensor)
-  tensor = ZeroPadding2D(padding=padding, data_format='channels_first')(tensor)
-  if cv2_name == None:
+  if padding == None:
     return tensor
-  tensor = Conv2D(cv2_out, cv2_filter, strides=cv2_strides, data_format='channels_first', name=cv2_name)(tensor)
-  tensor = BatchNormalization(axis=1, epsilon=0.00001, name=bn2_name)(tensor)
+  tensor = ZeroPadding2D(padding=padding, data_format='channels_first')(tensor)
+  if cv2_out == None:
+    return tensor
+  tensor = Conv2D(cv2_out, cv2_filter, strides=cv2_strides, data_format='channels_first', name=layer+'_conv'+'2')(tensor)
+  tensor = BatchNormalization(axis=1, epsilon=0.00001, name=layer+'_bn'+'2')(tensor)
   tensor = Activation('relu')(tensor)
   return tensor
